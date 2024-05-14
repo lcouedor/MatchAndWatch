37715c0e50d7e5837e311bc8b7278d5cefecb4a7
lcouedor <leo.couedor@gmail.com>
2024-05-14 18:16:05 +0200

    .

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.matchWatch.app',
+  appName: 'Match&Watch',
+  webDir: 'dist'
+};
+
+export default config;
