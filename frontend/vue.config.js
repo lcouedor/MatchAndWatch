ca6af8f69f766d9ff23935240deb48e9de20c3b1
lcouedor <leo.couedor@gmail.com>
2025-06-20 11:01:53 +0200

    fix déploiement gh

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,12 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: './',
   devServer: {
     host: "localhost",
     port: 8080,
     allowedHosts: 'all',
     compress: true,
   },
-
-  
 });
