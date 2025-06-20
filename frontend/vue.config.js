b8b6cc6d2a2939aa083d84a42fbff5e631cf714b
lcouedor <leo.couedor@gmail.com>
2025-06-20 11:10:05 +0200

    fix déploiement gh

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './',
+  publicPath: '/MatchAndWatch/',
   devServer: {
     host: "localhost",
     port: 8080,
